8a7689c7cbbd051c424f70b105efe24b4fba3c23
Yoonchul Nam <yooncn010@me.com>
2021-12-03 01:48:01 +1030

    res.json

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const user = {
+    id: ["woo", "rim", "IT"],
+    pw: ["123", "234", "345"],
+};
+
 const output = {
     home: (req, res) => {
         res.render("home/home");
@@ -7,13 +12,28 @@ const output = {
     login: (req, res) => {
         res.render("home/login");
     },
-}
+};
 
 const process = {
-    login: (req, res) =>{
-        console.log(req.body);
+    login: (req, res) => {
+        const id = req.body.id,
+         pw = req.body.pw;
+
+        if (user.id.includes(id)) {
+            const idx = user.id.indexOf(id);
+            if (user.pw[idx] === pw) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "Login Failure",
+        });
     }
-}
+};
 
 
 module.exports = {
