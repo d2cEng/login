0b2ca86f32bb40622a29ee890a2ab8ec8724cec4
Yoonchul Nam <yooncn010@me.com>
2021-12-04 02:06:02 +1030

    final version

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -7,7 +7,7 @@ class UserStorage {
         return new Promise((resolve, reject) => {
             const query = "SELECT * FROM users WHERE id = ?";
             db.query(query, [id], (err, data) => {
-                if (err) reject(err);
+                if (err) reject(`${err}`);
                 else resolve(data[0]);
             });
         });
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -5,15 +5,15 @@ const UserStorage = require("../../models/UserStorage");
 const logger = require("../../config/logger");
 const output = {
     home: (req, res) => {
-        logger.info(`GET / 200 "move to Home page"`);
+        logger.info(`GET / 304 "move to Home page"`);
         res.render("home/index");
     },
     login: (req, res) => {
-        logger.info(`GET /login 200 "move to login page"`);
+        logger.info(`GET /login 304 "move to login page"`);
         res.render("home/login");
     },
     register: (req, res) => {
-        logger.info(`GET /register 200 "move to signup page"`);
+        logger.info(`GET /register 304 "move to signup page"`);
         res.render("home/register");
     },
 };
@@ -22,28 +22,24 @@ const process = {
     login: async (req, res) => {
         const user = new User(req.body);
         const response = await user.login();
-        if (response.err)
-            logger.error(
-                `POST /login 200 Response: "success: ${response.success}, msg: ${response.err}"`
-            );
-        else
-            logger.info(
-                `POST /login 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-            );
-        return res.json(response);
+        const url = {
+            method: "POST",
+            path: "/login",
+            status: response.err ? 400 : 200,
+        };
+        log(response, url);
+        return res.status(url.status).json(response);
     },
     register: async (req, res) => {
         const user = new User(req.body);
         const response = await user.register();
-        if (response.err)
-            logger.error(
-                `POST /register 200 Response: "success: ${response.success}, msg: ${response.err}"`
-            );
-        else
-            logger.info(
-                `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-            );
-        return res.json(response);
+        const url = {
+            method: "POST",
+            path: "/register",
+            status: response.err ? 409 : 201,
+        };
+        log(response, url);
+        return res.status(url.status).json(response);
     },
 };
 
@@ -52,3 +48,15 @@ module.exports = {
     output,
     process,
 };
+
+const log = (response, url) => {
+    if (response.err) {
+        logger.error(
+            `${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.err}`
+        );
+    } else {
+        logger.info(
+            `${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.msg || ""}`
+        );
+    }
+}
