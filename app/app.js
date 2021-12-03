5ea1af7e8ff403db483120e7d189ca8347ab81e1
Yoonchul Nam <yooncn010@me.com>
2021-12-03 16:02:30 +1030

    front

--- app/app.js
+++ app/app.js
@@ -13,6 +13,8 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
+
+// To solve the issue not read Data having other characters such as Korean or empty space properly
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", home); // use, to register middleware
